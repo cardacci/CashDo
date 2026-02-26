7934640dc5c15239ca5bbad1ddaff027b7dcc4ab
Gabriel Cardacci <gabrielcardacci@gmail.com>
2026-02-26 00:53:14 -0300

    feat: add API error cooldown

--- src/store/useErrorStore.ts
+++ src/store/useErrorStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { API_ERROR_COOLDOWN } from '../constants';
 import { StorageErrorType } from '../types';
 
 /* ===== Types & Interfaces ===== */
@@ -9,13 +10,27 @@ export interface StorageError {
 
 interface ErrorState {
 	clearStorageError: () => void;
+	lastApiErrorAt: number;
 	setStorageError: (error: StorageError) => void;
 	storageError: StorageError | null;
 }
 
 /* ===== Store ===== */
-export const useErrorStore = create<ErrorState>()((set) => ({
+export const useErrorStore = create<ErrorState>()((set, get) => ({
 	clearStorageError: () => set({ storageError: null }),
-	setStorageError: (storageError: StorageError) => set({ storageError }),
+	lastApiErrorAt: 0,
+	setStorageError: (storageError: StorageError) => {
+		if (storageError.type === StorageErrorType.Api) {
+			const now = Date.now();
+
+			if (now - get().lastApiErrorAt < API_ERROR_COOLDOWN) {
+				return;
+			}
+
+			set({ lastApiErrorAt: now, storageError });
+		} else {
+			set({ storageError });
+		}
+	},
 	storageError: null
 }));
